72b61ebf369c0857586df7dab04d23e67dca4a66
kipply <carolyfisher@gmail.com>
2019-03-10 18:40:33 -0400

    Updates

--- precache-manifest.9b05c8cc50e8afc63ab7f94e0b603e47.js
+++ precache-manifest.9b05c8cc50e8afc63ab7f94e0b603e47.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/engcalc/static/js/runtime~main.bb94353f.js"
   },
   {
-    "revision": "db70352f92d617d26fc4",
-    "url": "/engcalc/static/js/main.799e7da8.chunk.js"
+    "revision": "ebdfc93dfae4160b76d2",
+    "url": "/engcalc/static/js/main.5f6e7412.chunk.js"
   },
   {
     "revision": "a3db0e179296cf1204d8",
     "url": "/engcalc/static/js/2.4fcf8da3.chunk.js"
   },
   {
-    "revision": "db70352f92d617d26fc4",
+    "revision": "ebdfc93dfae4160b76d2",
     "url": "/engcalc/static/css/main.c0bbd8e5.chunk.css"
   },
   {
-    "revision": "3b6b17ba46939449ee9b42d0ddc23413",
+    "revision": "f86a7f8c284f9482b1fe3238242a02c6",
     "url": "/engcalc/index.html"
   }
 ];
