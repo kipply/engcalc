7a7c2517eb48145ec56df1432e23aa0f5427400e
kipply <carolyfisher@gmail.com>
2019-03-10 11:30:47 -0400

    mmmm cooooode

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="row1"> 
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
