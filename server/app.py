ee74145a5dea1653fa7ef2b35cd8c25633983a8e
kipply <carolyfisher@gmail.com>
2019-03-10 15:56:50 -0400

    update server for cors

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import request
 import subprocess
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/evaluate")
 def evaluate():
